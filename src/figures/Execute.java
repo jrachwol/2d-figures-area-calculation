026ff18cba70d1d5ce915a1b2b66d2b1324c5c00
janusz.rachwol <janusz.rachwol@gmail.com>
2019-08-29 07:20:34 +0200

    Dodaje info o figurze dla której obliczone pole powierzchni.

--- src/figures/Execute.java
+++ src/figures/Execute.java
@@ -5,19 +5,19 @@ public class Execute {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Circle circle = new Circle (10);
-		System.out.println(circle.calculateArea());
+		System.out.println("Pole ko³a = " + circle.calculateArea());
 		
 		Rectangle rectangle = new Rectangle (5, 4);
-		System.out.println(rectangle.calculateArea());
+		System.out.println("Pole prostok¹ta = " + rectangle.calculateArea());
 		
 		Square square = new Square(5);
-		System.out.println(square.calculateArea());
+		System.out.println("Pole kwadratu = " + square.calculateArea());
 		
 		Trapezoid trapezoid = new Trapezoid(1, 2, 3, 4);
-		System.out.println(trapezoid.calculateArea());
+		System.out.println("Pole trapezoidu = " + trapezoid.calculateArea());
 		
 		Triangle triangle = new Triangle(20, 30);
-		System.out.println(triangle.calculateArea());
+		System.out.println("Pole trójk¹ta = " + triangle.calculateArea());
 	}	
 	
 }
